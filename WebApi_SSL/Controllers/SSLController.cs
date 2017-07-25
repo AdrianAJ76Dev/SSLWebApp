bb8e941b4c9aca1ab824caba97e4de20c0183b61
AdrianAJ76Dev <AdrianAJ76Dev@gmail.com>
2017-07-25 00:32:32 -0400

    Pulling together the individual parts of my Sole Source Letter Web Api

---
+++ WebApi_SSL/Controllers/SSLController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CBDocumentData;
+
+namespace WebApi_SSL.Controllers
+{
+    public class SSLController : ApiController
+    {
+        // POST: api/SSL
+        public void Post([FromBody]SSL SSLMembers)
+        {
+            /* 
+             * Step #1
+             * Turn object into XML by passing the object to a library
+             * 
+             */
+            SSL.AddDataSSL(SSLMembers);
+        }
+
+        // PUT: api/SSL_/5
+        public void Put(int id, [FromBody]SSL SSLMembers)
+        {
+
+        }
+    }
+}
